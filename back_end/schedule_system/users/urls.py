3b246a088112471441b7e40fb49580c1e15d5bfd
WinwaveJ <winwave1004@gmail.com>
2025-02-24 12:39:37 +0800

    用户管理模块初步完成，实现了更新用户信息和修改用户密码的基本功能，修改了用户注册的密码验证逻辑

--- back_end/schedule_system/users/urls.py
+++ back_end/schedule_system/users/urls.py
@@ -1,8 +1,10 @@
 # users/urls.py
 from django.urls import path
-from .views import UserRegistrationView, user_login
+from .views import UserRegistrationView, user_login, user_update, change_password
 
 urlpatterns = [
     path("register/", UserRegistrationView.as_view(), name="register"),
     path("login/", user_login, name="login"),
+    path("<int:user_id>/", user_update, name="user-update"),
+    path("<int:user_id>/password/", change_password, name="change-password"),
 ]
